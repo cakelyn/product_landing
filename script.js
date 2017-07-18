57d1bdc60daa4f6e2381b0de58262b4e59408905
Katelyn Stamas <cakelyn@gmail.com>
2017-07-18 14:10:11 -0400

    fix video and media query

--- script.js
+++ script.js
@@ -1,43 +1,7 @@
 $(document).ready(function() {
 
-  var nav = $("#nav-bar"),
-      mobile = $("#nav-mobile"),
-      links = $(".nav-link");
+  var nav = $("#nav-bar");
 
   nav.sticky({topSpacing: 0});
 
 });
-
-/**
-
-$(window).resize(function() {
-    if ($(window).width() < 810) {
-      mobile.show();
-      links.hide();
-    } else {
-      mobile.hide();
-      links.show();
-    }
-  });
-
-  mobile.onClick(function() {
-    showMenu();
-  });
-
-  function showMenu() {
-
-  }
-
-  function hideMenu() {
-
-  }
-
-<i id="mobile-icon" class="fa fa-bars fa-2x" aria-hidden="true"></i>
-
-#mobile-icon {
-  display: hidden;
-  color: #F9FDFF;
-}
-
-<script src="https://use.fontawesome.com/0259583fa8.js"></script>
- */
